92f19b5d6101f6fe8b775c7e4e707dc008039ec3
Yury Kliachko <uraklechko@gmail.com>
2018-10-21 20:57:12 +0300

    Filter methods for NotificationModel created. Relationship reference has been moved to the UserModel to comply with One-to-Many scheme (on user has many notifications)

--- models/notification.py
+++ models/notification.py
@@ -4,8 +4,24 @@ class NotificationModel(db.Model):
     __tablename__ = 'notifications'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    user = db.relationship('UserModel', lazy='dynamic')
     name = db.Column(db.String(80))
     date = db.Column(db.Date())
 
-    def __init__(self):
+    def __repr__(self):
+        return f"Notification ({self.name}, {self.date})"
+
+    def __init__(self, name, date):
+        self.name = name
+        self.date = date
+
+    @classmethod
+    def find_by_name(cls, name):
+        return cls.query.filter_by(name=name).first()
+
+    @classmethod
+    def find_by_date(cls, date):
+        return cls.query.filter_by(date=date).first()
+
+    @classmethod
+    def find_by_user_id(cls, user_id):
+        return cls.query.filter_by(user=user_id).all()
