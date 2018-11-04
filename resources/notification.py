edeee5b87df1dbcd75944f813f8d00d91441bde9
stivo32 <stivo32@mail.ru>
2018-11-04 19:46:28 +0300

    get all notifications with help of current_identity.id

--- resources/notification.py
+++ resources/notification.py
@@ -16,7 +16,14 @@ class Notification(Resource):
     def delete(self):
         pass
 
+
 class NotificationList(Resource):
-    #@jwt_required()
-    def get(self, user_id):
-        return {"notifications": [notification.jsonify() for notification in NotificationModel.filter_by(user_id=user_id).all()]}
+    @jwt_required()
+    def get(self):
+        return {
+            "notifications": [
+                notification.jsonify()
+                for notification
+                in NotificationModel.filter_by(user_id=current_identity.id).all()
+            ]
+        }
