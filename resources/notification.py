69b6b8fb496e25378a9963668c175407a54562ba
stivo32 <stivo32@mail.ru>
2018-11-04 14:16:55 +0300

    filled resources. User is same with course.

--- resources/notification.py
+++ resources/notification.py
@@ -1,4 +1,23 @@
 from flask_restful import Resource
+from flask_jwt import jwt_required
+from models.notification import NotificationModel
+
 
 class Notification(Resource):
-    pass
+    @jwt_required()
+    def get(self):
+        pass
+
+    @jwt_required()
+    def post(self):
+        pass
+
+    @jwt_required()
+    def delete(self):
+        pass
+
+
+class NotificationList(Resource):
+    @jwt_required()
+    def get(self):
+        pass
