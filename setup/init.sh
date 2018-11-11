9b9f99c7caea0de6204e6bac21fb0569aedb7753
stivo32 <stivo32@mail.ru>
2018-11-11 19:39:10 +0300

    пара правок для путей. Вставил переменную $HOME вместо абсолютного пути. Это позволит не менять пути на другом юзере.

--- setup/init.sh
+++ setup/init.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sudo rm -rf /etc/nginx/sites-enabled/default
-sudo ln -s /home/modesty/PythonWeb/git_notification/setup/nginx_notifications.conf /etc/nginx/sites-enabled/nginx_notifications.conf
+sudo rm -rf /etc/nginx/sites-enabled/*
+sudo ln -s $HOME/PythonWeb/git_notification/setup/nginx_notifications.conf /etc/nginx/sites-enabled/nginx_notifications.conf
